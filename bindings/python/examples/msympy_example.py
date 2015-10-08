4e623457a2bc0e6721f1db63a6976b1cbeb20b4f
mcodev31 <mcodev31@gmail.com>
2015-10-08 13:17:59 +0200

    xyz files for python

--- bindings/python/examples/msympy_example.py
+++ bindings/python/examples/msympy_example.py
@@ -1,31 +1,37 @@
 #!/usr/bin/python
 
-import msympy
+import msympy, argparse
 
-elements = [msympy.Element(mass = 1.0, charge = 1, name = "H", coordinates = [1, 0, 0]),
-		msympy.Element(mass = 1.0, charge = 1, name = "H", coordinates = [0, 1, 0]),
-		msympy.Element(mass = 1.0, charge = 1, name = "H", coordinates = [0, 0, 1.0000001])]
 
+
+def read_xyz(fin):
+    length = int(fin.readline())
+    comment = fin.readline()[:-1]
+    elements = []
+    for i in range(0,length):
+        line = fin.readline().split()
+        elements.append(msympy.Element(name = line[0], coordinates = map(float, line[1:4])))
+
+    return (elements, comment)
+
+def write_xyz(fout, elements, title):
+    fout.write("%d\n%s\n" % (len(elements), title))
+    for e in elements:
+        v = e.coordinates
+        fout.write("%s %.15g %.15g %.15g\n" % (e.name, v[0], v[1], v[2]))
+
+parser = argparse.ArgumentParser()
+parser.add_argument('infile', type=argparse.FileType('r'))
+parser.add_argument('outfile', type=argparse.FileType('w'))
+args = parser.parse_args()
+
+(elements, comment) = read_xyz(args.infile)
 ctx = msympy.Context()
-
 ctx.elements = elements
+point_group_name = ctx.findSymmetry()
+elements = ctx.symmetrizeElements()
+write_xyz(args.outfile, elements, comment + " symmetrized by libmsympy according to point group " + point_group_name)
 
-name = ctx.findSymmetry()
 
-print "Python has received point group %s." % name
 
-symelements = ctx.symmetrizeElements()
-for element in symelements:
-	print element.coordinates
-
-elements[0].name = "C"
-elements[1].name = "C"
-elements[0].coordinates = (0.0,0.0,1.0)
-elements[1].coordinates = (0.0,0.0,-1.0)
-
-ctx.elements = [elements[0],elements[1]]
-
-name = ctx.findSymmetry()
-
-print "Python has received point group %s." % name
 
